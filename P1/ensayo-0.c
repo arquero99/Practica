6e821603a927f942a3d839b8387cfa816003cde5
Juan Arquero <j.arquerog@gmail.com>
2021-09-19 01:48:02 +0200

    Documentos y archivos asociados a la primera sesión de prácticas

---
+++ P1/ensayo-0.c
@@ -0,0 +1,26 @@
+#include <xc.h>
+
+// See /opt/microchip/xc8/v<version>/docs/chips/16f886.html
+// for details on #pragma config
+
+#pragma config CPD = OFF, BOREN = OFF, IESO = OFF, DEBUG = OFF, FOSC = HS
+#pragma config FCMEN = OFF, MCLRE = ON, WDTE = OFF, CP = OFF, LVP = OFF
+#pragma config PWRTE = ON, BOR4V = BOR21V, WRT = OFF
+
+
+void main(void)
+{ char x;
+
+  OSCCON = 0b00001000; // External cristal
+
+  x = 0;
+  ANSEL = 0;	// Configure as digital all CAD channels
+  ANSELH = 0;
+  TRISA = 0; 	// Configure port A as output
+  TRISB = 0;
+
+  while(1)
+  { PORTB = x;
+    x += 1;
+   } 
+}
