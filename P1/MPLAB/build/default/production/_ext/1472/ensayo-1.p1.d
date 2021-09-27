3029061bdb3458c6ce10c1d7b4fa66ae3964aa3f
Juan <j.arquerog@gmail.com>
2021-09-27 14:34:47 +0200

    Subido ejercicio 1 de la sesion 1. No compila debido a redefinición del main. Preguntar en clase

---
+++ P1/MPLAB/build/default/production/_ext/1472/ensayo-1.p1.d
@@ -0,0 +1,3 @@
+ build/default/production/_ext/1472/ensayo-1.d  \
+ build/default/production/_ext/1472/ensayo-1.p1:  \
+ ../ensayo-1.c 
--- P1/ensayo-0.c
+++ P1/ensayo-0.c
@@ -1,4 +1,3 @@
-#include <xc.h>
 
 // See /opt/microchip/xc8/v<version>/docs/chips/16f886.html
 // for details on #pragma config
@@ -6,6 +5,8 @@
 #pragma config CPD = OFF, BOREN = OFF, IESO = OFF, DEBUG = OFF, FOSC = HS
 #pragma config FCMEN = OFF, MCLRE = ON, WDTE = OFF, CP = OFF, LVP = OFF
 #pragma config PWRTE = ON, BOR4V = BOR21V, WRT = OFF
+
+#include <xc.h>
 
 
 void main(void)
