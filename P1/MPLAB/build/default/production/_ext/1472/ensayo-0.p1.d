da4b878a1f4129e7e6b1ac1759500f1d2003718a
Juan <j.arquerog@gmail.com>
2021-09-28 12:42:34 +0200

    Actualizados parametros de simulación de sesion 1

--- P1/MPLAB/build/default/production/_ext/1472/ensayo-0.p1.d
+++
@@ -1,3 +0,0 @@
- build/default/production/_ext/1472/ensayo-0.d  \
- build/default/production/_ext/1472/ensayo-0.p1:  \
- ../ensayo-0.c 
--- P1/MPLAB/nbproject/Makefile-default.mk
+++ P1/MPLAB/nbproject/Makefile-default.mk
@@ -45,17 +45,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=../ensayo-0.c ../ensayo-1.c
+SOURCEFILES_QUOTED_IF_SPACED=../ensayo-1.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/1472/ensayo-0.p1 ${OBJECTDIR}/_ext/1472/ensayo-1.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/1472/ensayo-0.p1.d ${OBJECTDIR}/_ext/1472/ensayo-1.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/1472/ensayo-1.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/1472/ensayo-1.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/_ext/1472/ensayo-0.p1 ${OBJECTDIR}/_ext/1472/ensayo-1.p1
+OBJECTFILES=${OBJECTDIR}/_ext/1472/ensayo-1.p1
 
 # Source Files
-SOURCEFILES=../ensayo-0.c ../ensayo-1.c
+SOURCEFILES=../ensayo-1.c
 
 
 CFLAGS=
@@ -78,14 +78,6 @@ MP_PROCESSOR_OPTION=16F886
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
-${OBJECTDIR}/_ext/1472/ensayo-0.p1: ../ensayo-0.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} ${OBJECTDIR}/_ext/1472 
-	@${RM} ${OBJECTDIR}/_ext/1472/ensayo-0.p1.d 
-	@${RM} ${OBJECTDIR}/_ext/1472/ensayo-0.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=none  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,+osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/1472/ensayo-0.p1  ../ensayo-0.c 
-	@-${MV} ${OBJECTDIR}/_ext/1472/ensayo-0.d ${OBJECTDIR}/_ext/1472/ensayo-0.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/_ext/1472/ensayo-0.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
-	
 ${OBJECTDIR}/_ext/1472/ensayo-1.p1: ../ensayo-1.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/_ext/1472 
 	@${RM} ${OBJECTDIR}/_ext/1472/ensayo-1.p1.d 
@@ -95,14 +87,6 @@ ${OBJECTDIR}/_ext/1472/ensayo-1.p1: ../ensayo-1.c  nbproject/Makefile-${CND_CONF
 	@${FIXDEPS} ${OBJECTDIR}/_ext/1472/ensayo-1.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 else
-${OBJECTDIR}/_ext/1472/ensayo-0.p1: ../ensayo-0.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} ${OBJECTDIR}/_ext/1472 
-	@${RM} ${OBJECTDIR}/_ext/1472/ensayo-0.p1.d 
-	@${RM} ${OBJECTDIR}/_ext/1472/ensayo-0.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,+osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/_ext/1472/ensayo-0.p1  ../ensayo-0.c 
-	@-${MV} ${OBJECTDIR}/_ext/1472/ensayo-0.d ${OBJECTDIR}/_ext/1472/ensayo-0.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/_ext/1472/ensayo-0.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
-	
 ${OBJECTDIR}/_ext/1472/ensayo-1.p1: ../ensayo-1.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/_ext/1472 
 	@${RM} ${OBJECTDIR}/_ext/1472/ensayo-1.p1.d 
