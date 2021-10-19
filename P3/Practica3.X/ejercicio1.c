3e75c7de4c92992fee02cd3655c3d85e9f5e7395
Juan <j.arquerog@gmail.com>
2021-10-19 03:14:10 +0200

    Depurado codigo para sesion 3·

--- P3/Practica3.X/ejercicio1.c
+++ P3/Practica3.X/ejercicio1.c
@@ -1,6 +1,6 @@
 /* 
  * File:   ejercicio1.c
- * Author: alumno
+ * Author: Juan Arquero
  *
  * Created on 4 de octubre de 2021, 13:28
  */
@@ -11,10 +11,31 @@
 #pragma config FCMEN = OFF, MCLRE = ON, WDTE = OFF, CP = OFF, LVP = OFF
 #pragma config PWRTE = ON, BOR4V = BOR21V, WRT = OFF
 
+void init_CAD();
+void init_t0();
+void getAnalog(void);
+void dumpData(void);
+void __interrupt() CAD_int(void);
+void interrupt t0int (void);
 
-/*
- * 
- */
+int numInterruptT0=0;
+
+void __interrupt() CAD_int(void){
+    dumpData();
+    PIR1bits.ADIF=0;    //Flag de interrupción CAD a 0
+    //getAnalog();    //Llamada recursiva que actuará como bucle infinito
+}
+
+void interrupt t0int (void) //Interrupcion que saca el contenido por puertoB cada 500ns
+{
+    TMR0 =157;
+    numInterruptT0++;
+    if(numInterruptT0>=99){
+        dumpData();
+        numInterruptT0 = 0;
+    }
+    INTCONbits.T0IF=0; //Resetea la interrupción
+}
 
 void init_CAD(){
     //Configuración de puerto de entrada
@@ -27,33 +48,53 @@ void init_CAD(){
     ADCON1bits.VCFG1=0;     //Ground como Vref-
     ADCON1bits.VCFG0=1;     //Vdd como Vref+
     //Configuración ADCON0
-    ADCON0bits.ADCS=0b10;   //Para configurar reloj a Fosc/32 (20Mhz, 1.6uS)
+    ADCON0bits.ADCS=0b10;   //Para configurar reloj a Fosc/32 (20Mhz, 1.6uS) PAGINA 10 ESPECIFICO
     ADCON0bits.CHS=0b0000;  //Selecciona entrada AN0
     ADCON0bits.ADON=1;      //Enciende conversor
 }
-void __interrupt() CAD_int(void){
-    PIR1bits.ADIF=0;    //Flag de interrupción CAD a 0
-    //getAnalog();    //Llamada recursiva que actuará como bucle infinito
+
+void init_t0()
+{
+    OPTION_REGbits.PS=0b111;
+    OPTION_REGbits.T0CS=0;
+    OPTION_REGbits.PSA=0;
+    INTCONbits.T0IE=1;
 }
 
 void getAnalog(void){
-for(int i=0;i++;i<16){
-            __nop(); //Para respetar el tiempo de adquisicionX2 insertamos 16 operaciones vacías
+for(int i=0;i++;i<5){
+            __nop(); //Para respetar el tiempo de carga del condensador insertamos 5 operaciones vacías
+        }
+for(int i=0;i++;i<88){
+            __nop(); //Para respetar el tiempo de adquisicion X11 insertamos 88 operaciones vacías
         }
         //AQUI REALIZAMOS CONVERSION
 ADCON0bits.GO=1;
-while(!PIR1bits.ADIF) //Esperamos hasta que llegue interrupción de conversor A/D
-{
 }
-PORTB=ADRESL;   //Pasamos a Puerto B la salida de converosr A/D
+
+void dumpData(void){
+//Pasamos a Puerto B la salida de converosr A/D. Lo suyo es hacer PORTB[7]=ADRESH[0], PORB[6]=ADRESL[7] .....
+PORTBbits.RB0=ADRESL.1;
+PORTBbits.RB1=ADRESL.2;
+PORTBbits.RB2=ADRESL.3;
+PORTBbits.RB3=ADRESL.4;
+PORTBbits.RB4=ADRESL.5;
+PORTBbits.RB5=ADRESL.6;
 }
 
 
 int main() {
     PIE1bits.ADIE=1;    //Interrupcion CAD habilitada
-    INTCONbits.PEIE=1;  //Interrupcion de periféricos habilitada
+    INTCONbits.PEIE=1;  //Interrupcion de periféricos habilitada        PREGUNTAR!!!!!!!!!!!!!!!!!
     INTCONbits.GIE=1;   //Interrupciones habilitadas
-    
+    init_CAD(); //Configuracion de puertos y encendido del CAD
+    init_t0();
+    while(1){
+    getAnalog();
+    //while(ADCON0bits.GO){}  //Espera mientras la conversion está en curso (Metodo sin usar interrupciones)
+    while(!PIR1bits.ADIF) {}  //Esperamos hasta que llegue interrupción de conversor A/D
+    while(!INTCONbits.T0IF){}                       //Espera de 500ms
+    }
     return (EXIT_SUCCESS);
 }
 
--- P3/Practica3.X/nbproject/Makefile-default.mk
+++ P3/Practica3.X/nbproject/Makefile-default.mk
@@ -45,17 +45,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=
+SOURCEFILES_QUOTED_IF_SPACED=ejercicio1.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=
-POSSIBLE_DEPFILES=
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/ejercicio1.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/ejercicio1.p1.d
 
 # Object Files
-OBJECTFILES=
+OBJECTFILES=${OBJECTDIR}/ejercicio1.p1
 
 # Source Files
-SOURCEFILES=
+SOURCEFILES=ejercicio1.c
 
 
 CFLAGS=
@@ -78,7 +78,23 @@ MP_PROCESSOR_OPTION=16F886
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/ejercicio1.p1: ejercicio1.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR} 
+	@${RM} ${OBJECTDIR}/ejercicio1.p1.d 
+	@${RM} ${OBJECTDIR}/ejercicio1.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=none  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,+osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/ejercicio1.p1  ejercicio1.c 
+	@-${MV} ${OBJECTDIR}/ejercicio1.d ${OBJECTDIR}/ejercicio1.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/ejercicio1.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 else
+${OBJECTDIR}/ejercicio1.p1: ejercicio1.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR} 
+	@${RM} ${OBJECTDIR}/ejercicio1.p1.d 
+	@${RM} ${OBJECTDIR}/ejercicio1.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,+osccal,-resetbits,-download,-stackcall,+clib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/ejercicio1.p1  ejercicio1.c 
+	@-${MV} ${OBJECTDIR}/ejercicio1.d ${OBJECTDIR}/ejercicio1.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/ejercicio1.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 endif
 
 # ------------------------------------------------------------------------------------
